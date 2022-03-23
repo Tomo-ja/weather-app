dcc0c2b4247a6355a8a2cddc445e383e94e9a3be
Tomo-ja <tomonao.hj@gmail.com>
2022-03-22 21:39:18 -0700

    import and use api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
+import weatherData from './apis/weatherData';
+import setLocation from './apis/setLocation';
 import './App.css';
 
 function App() {
+  const getLocation = async()=>{
+    try{
+      const location = await setLocation.get('geo/1.0/direct?q=London&limit=&appid=748752212852e7cf71bcfcf6066d4ab0')
+      const { lat, lon} = location.data[0]
+      console.log(`${lat} and ${lon}`)
+    }catch(err){
+      console.log(err)
+    }
+  }
+  const getWeatherData = async()=>{
+    try{
+      const data = await weatherData.get(`data/2.5/weather?lat=35&lon=139&appid=748752212852e7cf71bcfcf6066d4ab0`)
+      console.log(data.data.weather[0].main)
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
-    <div className="App">
+    <div className="App" onClick={getWeatherData}>
       here
     </div>
   );
