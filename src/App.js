bc3a7e9167c38becbe12340710a7db0e0f957085
Tomo-ja <tomonao.hj@gmail.com>
2022-03-26 17:49:42 -0700

    include google fonts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import weatherData from './apis/weatherData';
-import setLocation from './apis/setLocation';
+import React from 'react';
+import weatherDataApi from './apis/weatherData';
+import setLocationApi from './apis/setLocation';
 import Header from './components/Header';
 import Main from './components/Main';
 import './App.css';
 
+export const locationData = React.createContext()
+export const weatherData = React.createContext()
 
 function App() {
   const getLocation = async()=>{
     try{
-      const location = await setLocation.get('geo/1.0/direct?q=London&limit=&appid=748752212852e7cf71bcfcf6066d4ab0')
+      const location = await setLocationApi.get('geo/1.0/direct?q=London&limit=&appid=748752212852e7cf71bcfcf6066d4ab0')
       const { lat, lon} = location.data[0]
       console.log(`${lat} and ${lon}`)
     }catch(err){
@@ -17,7 +20,7 @@ function App() {
   }
   const getWeatherData = async()=>{
     try{
-      const data = await weatherData.get(`data/2.5/weather?lat=35&lon=139&appid=748752212852e7cf71bcfcf6066d4ab0`)
+      const data = await weatherDataApi.get(`data/2.5/weather?lat=35&lon=139&appid=748752212852e7cf71bcfcf6066d4ab0`)
       console.log(data.data.weather[0].main)
     }catch(err){
       console.log(err)
@@ -25,9 +28,15 @@ function App() {
     }
   }
   return (
-    <div className="App" onClick={getWeatherData}>
-      <Header/>
-      <Main />
+    <div className="app" onClick={getWeatherData}>
+      <locationData.Provider value="Vancouver" >
+        <weatherData.Provider>
+          <Header/>
+        </weatherData.Provider>
+      </locationData.Provider>
+      <weatherData.Provider>
+        <Main />
+      </weatherData.Provider>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import '../css/header.css';
+import { locationData } from "../App";
+
 
 export default function Header(){
+	const location = React.useContext(locationData)
 	return(
-		<div className="header">
-			header will be here
+		<div className="header outer">
+			<div className="header_hamburger-menu">
+				<span className="header_hamburger-menu_btn"></span>
+			</div>
+			<h1 className="header_location-name location-name">{location}</h1>
 		</div>
 	)
 }
