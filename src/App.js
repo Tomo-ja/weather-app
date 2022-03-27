3249ce9d9609c476e719aa2fbf08da56a079ae76
Tomo-ja <tomonao.hj@gmail.com>
2022-03-26 20:37:37 -0700

    change structure

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import weatherDataApi from './apis/weatherData';
-import setLocationApi from './apis/setLocation';
 import Header from './components/Header';
 import Main from './components/Main';
 import './App.css';
 
-export const CurrentLocation = React.createContext()
 export const WeatherData = React.createContext()
 
 
 
 function App() {
-  const [locationNow, setlocationNow] = React.useState({});
 
-
-  const getLocation = async()=>{
-    try{
-      const location = await setLocationApi.get('geo/1.0/direct?q=London&limit=&appid=748752212852e7cf71bcfcf6066d4ab0')
-      const { name, country, lat, lon} = location.data[0]
-      setlocationNow({
-        "cityName": name,
-        "country": country,
-        "lat": lat,
-        "lon": lon
-      })
-    }catch(err){
-      console.log(err)
-    }
-  }
   const getWeatherData = async()=>{
     try{
       const data = await weatherDataApi.get(`data/2.5/weather?lat=35&lon=139&appid=748752212852e7cf71bcfcf6066d4ab0`)
@@ -38,12 +20,10 @@ function App() {
     }
   }
   return (
-    <div className="app" onClick={getLocation}>
-      <CurrentLocation.Provider value={locationNow}>
+    <div className="app">
         {/* <weatherData.Provider> */}
           <Header/>
         {/* </weatherData.Provider> */}
-      </CurrentLocation.Provider>
       {/* <weatherData.Provider> */}
         <Main />
       {/* </weatherData.Provider> */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,38 @@
 import React from "react";
+import setLocationApi from '../apis/setLocation'
 import '../css/header.css';
 import reloadIcon from '../images/icon_reload.svg'
-import { CurrentLocation } from "../App";
 
 
 export default function Header(){
-	const location = React.useContext(CurrentLocation)
-	console.log(location.lat)
+	const [locationNow, setLocationNow] = React.useState({});
+
+
+	const getLocation = async()=>{
+		try{
+		  const location = await setLocationApi.get('geo/1.0/direct?q=London&limit=&appid=748752212852e7cf71bcfcf6066d4ab0')
+		  const { name, country, lat, lon} = location.data[0]
+		  setLocationNow({
+			"cityName": name,
+			"country": country,
+			"lat": lat,
+			"lon": lon
+		  })
+		}catch(err){
+		  console.log(err)
+		}
+	  }
+	
 	return(
 		<div className="outer header">
 			<div className="header_hamburger-menu">
 				<span className="header_hamburger-menu_btn"></span>
 			</div>
-			<h1 className="header_location-name location-name">{location.cityName}, {location.country}</h1>
-			<img className="icon header_icon-reload" src= {reloadIcon} alt="reload icon" />
+			<h1 className="header_location-name location-name">{locationNow.cityName}, {locationNow.country}</h1>
+			<img 	className="icon header_icon-reload"
+					src= {reloadIcon} 
+					alt="reload icon"
+					onClick={getLocation} />
 		</div>
 	)
 }
