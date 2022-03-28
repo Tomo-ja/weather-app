85269681a27cfdfc20c40fb01e7ef442ec82bc18
Tomo-ja <tomonao.hj@gmail.com>
2022-03-27 18:49:26 -0700

    design weekly

--- src/components/WeatherWeekly.js
+++ src/components/WeatherWeekly.js
@@ -1,9 +1,16 @@
-
+import WeatherWeeklyCard from "./WeatherWeeklyCard"
+import "../css/weatherWeekly.css"
 
 export default function WeatherWeekly(){
 	return(
-		<section>
-			week report
+		<section className="section weather-weekly">
+			<WeatherWeeklyCard/>
+			<WeatherWeeklyCard/>
+			<WeatherWeeklyCard/>
+			<WeatherWeeklyCard/>
+			<WeatherWeeklyCard/>
+			<WeatherWeeklyCard/>
+			<WeatherWeeklyCard/>
 		</section>
 	)
 }
